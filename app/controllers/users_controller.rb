767795788aaf7aae613b24c6b8ebf1f77f56fc51
Sean Esteva <seangeleno@gmail.com>
2015-11-03 14:22:14 -0800

    cleaned up users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
-    @definitions = @user.definitions
+    # @definitions = @user.definitions
   end
   def new
     @user = User.new
@@ -12,16 +12,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find( current_user[ :id ] )
   end
+
   def create
-    def create
-      @user = User.new( user_params )
-      if @user.save
-        session[:user_id] = @user.id.to_s
-        redirect_to user_path( @user )
-      else
-        render :new
-      end
+    @user = User.new( user_params )
+    if @user.save
+      session[:user_id] = @user.id.to_s
+      redirect_to user_path( @user )
+    else
+      render :new
     end
+  end
     def destroy
 		@user = User.find( params[ :id ] )
 		@user.destroy
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
   end
   private
     def user_params
-      params.require( :user ).permit( :name , :email , :avatar , :password , :password_confirmation )
+      params.require( :user ).permit( :f_name , :l_name , :email, :password , :password_confirmation )
     end
   end
-end
