fc6dc5faac2610c70f85a59565e51a8ccfbb5c67
Sean Esteva <seangeleno@gmail.com>
2015-11-05 13:34:53 -0800

    added dependent: :destroy to the USER model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :definitions
+  has_many :definitions, dependent: :destroy
   has_many :words
   has_many :definitions, :through => :words
   has_secure_password
